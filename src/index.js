2679b3c6cae93ae1eca34bbd74d9e5906898b327
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-11 18:37:47 -0300

    feat(checksTodoExists): implements middleware

--- src/index.js
+++ src/index.js
@@ -34,7 +34,26 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  const user = users.find(user => user.username === username);
+
+  if(!user)
+    return response.status(404).json({error: "User not found"});
+
+  if(!validate(id))
+    return response.status(400).json({error: "id not valid"});
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo)
+    return response.status(404).json({error: "no todo with this id was found"})
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
